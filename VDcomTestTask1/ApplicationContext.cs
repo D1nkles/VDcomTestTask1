9edcae6f834958118a78c22f55e5eacd37e0583e
Dinkles <isinetov@mail.ru>
2024-08-19 17:14:01 +0400

    Добавил в проект Entity Framework. Реализовал с его помощью подключение к БД в MS SQL Server Management. Обеспечил создание БД и ее сущностей, если таковых нет на стороне СУБД.

---
+++ VDcomTestTask1/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VDcomTestTask1.Entities;
+
+namespace VDcomTestTask1
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<IndividualEntity> Individuals { get; set; }
+        public DbSet<LegalEntity> Legals { get; set; }
+        public DbSet<ContractEntity> Contracts { get; set; }
+
+        public ApplicationContext() 
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ILIA\SQLEXPRESS;Database=VDComTask1DB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
